1a56b5076223d5a5a28808ec0d3394fc829bc123
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-12-01 12:39:23 +0000

    add option to not pass through fallback, default true

--- lua/nvim-mapper/init.lua
+++ lua/nvim-mapper/init.lua
@@ -18,13 +18,16 @@ end
 ---@param mode Mode[]|Mode: The mode or list of modes the mapping should apply to
 ---@param left string: left part of mapping
 ---@param right string|fun(fallback: unknown): string|nil Right part of mapping
----@param opts table?: options for our keymap
+---@param opts table: options for our keymap
 M.map_keymap = function(mode, left, right, opts)
     ---@type Mode[]
     mode = type(mode) == "table" and mode or { mode }
+    local fallback = opts.fallback
+    opts.fallback = nil
     local new_mapping = right
     for _, m in ipairs(mode) do
-        vim.keymap.set(m, left, M.gen_mapping(m, left, new_mapping), opts)
+        vim.keymap.set(m, left,
+            M.gen_mapping(m, left, new_mapping, fallback), opts)
     end
 end
 
@@ -35,7 +38,7 @@ end
 ---@field[3] string | fun(fallback: unknown|nil) right
 ---@field[4] table?: opts: options for the specific keymap that override options passed through map_keymap_list
 ---@param mappings Keymap[]: A list of keymaps follwing the `Keymap` type
----@param ext_opts table?: Options which will be additionally applied to every keymap in the given mapping list
+---@param ext_opts table: Options which will be additionally applied to every keymap in the given mapping list
 M.map_keymap_list = function(mappings, ext_opts)
     ---@param mapping Keymap
     vim.tbl_map(function(mapping)
@@ -43,7 +46,7 @@ M.map_keymap_list = function(mappings, ext_opts)
         local left = mapping[2]
         local right = mapping[3]
         local opts = mapping[4]
-        opts = vim.tbl_deep_extend("keep", opts or {}, ext_opts or {})
+        opts = vim.tbl_deep_extend("keep", opts or {}, ext_opts or {}) or {}
         M.map_keymap(mode, left, right, opts)
     end, mappings)
 end
@@ -52,7 +55,9 @@ end
 ---@param mode Mode
 ---@param left string
 ---@param right string|fun(fallback: function|nil)
-M.gen_mapping = function(mode, left, right)
+---@param fallback boolean? Whether you want the fallback to be passed through. Needed when you are passing a callback with a different signature. For example, vim.lsp.buf.rename. True by default
+M.gen_mapping = function(mode, left, right, fallback)
+    fallback = fallback == nil and true or fallback
     if type(right) == "string" then
         return function()
             send_keys_to_nvim_with_count(right)
@@ -70,6 +75,11 @@ M.gen_mapping = function(mode, left, right)
             end
         end
     end
+
+    if not fallback then
+        return mapping_or_default(right)
+    end
+
     ---@type string|function
     local prev_mapping
     local keymap_meta_info = vim.fn.maparg(left, mode, nil, true)
@@ -86,15 +96,10 @@ M.gen_mapping = function(mode, left, right)
         function()
             send_keys_to_nvim_with_count(prev_mapping)
         end
-    if debug.getinfo(right, "n").namewhat == "" then
-        return mapping_or_default(function()
-            right(prev_mapping)
-        end)
-    end
-    -- If named function, then don't pass our fallback through as it could take differen
-    -- arguments. E.g. we could not pass a function to lsp.buf.rename(...) because
-    -- it takes it's own arguments
-    return mapping_or_default(right)
+
+    return mapping_or_default(function()
+        right(prev_mapping)
+    end)
 end
 
 return M
